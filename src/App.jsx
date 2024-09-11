3a2c734301c23a1e52b8f68319d21d2e6572f45b
Siddharth-M-77 <siddharthmaddheshiya777@gmail.com>
2024-09-11 04:01:44 -0700

    done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from './Components/Sidebar'
+
+const App = () => {
+  return (
+    <div className='w-full min-h-screen bg-[#121212] p-10'>
+      <Sidebar/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div>
+      <h1 className='text-center text-4xl text-white '>heloooooooooooooooooo</h1>
+    </div>
+  )
+}
+
+export default Sidebar
